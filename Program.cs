60c58ef9ef81f48eda01b7b34c6f8d2ea6514d18
Sarah Walker <sarahsweetpie6@gmail.com>
2025-01-16 12:36:52 -0700

    added line to test github system

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.InteropServices;
+using Mission2;
+
+internal class Program
+{
+    //int totalRolls = 0;
+    private static void Main(string[] args)
+    {
+        Roll r = new Roll();
+
+        //ask user for desired number of rolls
+        Console.WriteLine("Welcome to the dice throwing simulator!\n");
+        Console.WriteLine("How many dice rolls would you like to simulate?");
+
+        //get input
+        int totalRolls = int.Parse(Console.ReadLine());
+
+        //call 2nd class to roll the dice
+        int[] rollResults = r.rollDice(totalRolls);
+
+        Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS \nEach \"*\" represents 1% of the total number of rolls.\nTotal number of rolls = " + totalRolls + ".\n");
+
+        //for each number possible to be rolled
+        //print the number
+        for (int index = 0; index < rollResults.Length; index++)
+        {
+            int rollSum = rollResults[index];
+            int product = rollSum * 100;
+            int percent = product / totalRolls;
+            string resultString = (index + 2) + ": ";
+
+            //calculate the percentage of the times it was rolled
+            //print an asterisk for each percent is was rolled
+            for (int i = 0; i < percent; i++)
+            {
+                resultString = resultString + "*";
+            }
+            Console.WriteLine(resultString + "\n");
+        }
+
+        Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+    }
+
+}
---
+++ Roll.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Mission2
+{
+	internal class Roll
+	{
+
+    //testing
+
+        public int[] results = new int[11];
+        public Roll()
+        {
+            for (int Count = 0; Count < 11; Count++)
+            {
+                results[Count] = 0;
+            }
+        }
+		public int[] rollDice(int totalRolls)
+		{
+            Random rnd = new Random();
+           
+            for (int Count = 0; Count < totalRolls; Count++)
+            {
+                //roll first dice
+                int dice1 = rnd.Next(0, 6) + 1;
+                //roll second dice
+                int dice2 = rnd.Next(0, 6) + 1;
+                //save sum of dice rolls
+                int rollSum = dice1 + dice2;
+                //store result
+                results[rollSum - 2] = results[rollSum - 2] + 1;
+            }
+
+            //pass back to first class
+            return results;
+
+        }
+    }
+}
